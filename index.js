4399d16baca06a4115b0bb42172ef189b647cce5
Sannamari <sannamari.pynnonen@tuni.fi>
2023-08-26 14:32:38 +0300

    Exercise 3.11 puhelinluettelo full stack

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(express.static('build'))
 
 let persons = [
     { 
@@ -95,7 +96,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
