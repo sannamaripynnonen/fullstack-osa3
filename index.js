808c9c1e01fae30ec653c80ba3dc3acbcf743506
Sannamari <sannamari.pynnonen@tuni.fi>
2023-08-29 21:31:04 +0300

    Exercise 3.13 puhelinluettelo ja tietokanta, step1

--- index.js
+++ index.js
@@ -1,42 +1,25 @@
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const cors = require('cors');
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(express.static('build'))
 
-let persons = [
-    { 
-        name: "Arto Hellas", 
-        number: "040-123456",
-        id: 1
-      },
-      { 
-        name: "Ada Lovelace", 
-        number: "39-44-5323523",
-        id: 2
-      },
-      { 
-        name: "Dan Abramov", 
-        number: "12-43-234345",
-        id: 3
-      },
-      { 
-        name: "Mary Poppendieck", 
-        number: "39-23-6423122",
-        id: 4
-      }
-]
+const password = 'ItULZLJ5ZPSxZGKi'
 
 const generateId = () => {
     return Math.floor(Math.random()*100)
 }
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+        res.json(persons)
+    })
 })
 
 app.get('/info', (req, res) => {
@@ -96,7 +79,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
